b4d83cb3d44acc2340e4b3d83321eb1dad35ec8c
github keita2580 <shiratori@aris-kk.co.jp>
2020-06-26 14:03:04 +0900

    追加

--- Swiftcalendar/ViewController.swift
+++ Swiftcalendar/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //80%まで縮小させて・・・
+        UIView.animate(withDuration: 0.3,
+                       delay: 1.0,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }, completion: { (Bool) in
+            
+        })
+        
+        //8倍まで拡大！
+        UIView.animate(withDuration: 0.2,
+                       delay: 1.3,
+                       options: UIView.AnimationOptions.curveEaseOut,
+                       animations: { () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 8.0, y: 8.0)
+                        self.imageView.alpha = 0
+        }, completion: { (Bool) in
+            //で、アニメーションが終わったらimageViewを消す
+            self.imageView.removeFromSuperview()
+            
+            //画面遷移
+            self.performSegue(withIdentifier: "next", sender: nil)
+        })
     }
 
-
 }
+
 
